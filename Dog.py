803e393809d41d55c0440d27011744b12c8539e2
c369578336 <369578336@qq.com>
2021-11-23 15:28:17 +0800

    test

---
+++ Dog.py
@@ -0,0 +1,17 @@
+class dog():
+    def __init__(self,name,age) -> None:
+        self.name=name
+        self.age=age
+        self.state='stand'
+    
+    def sit(self):
+        print('%s is sitting!'%(self.name))
+        self.state='sit'
+
+dogge=dog('fuck',2)
+print(dogge.name)
+print(dogge.state)
+dogge.sit()
+print(dogge.state)
+
+#反正也是test
